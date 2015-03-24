c55a1a4d266a07e0b67c093901f990e0e30f3990
BenaddiHajar <hajar_anissa@hotmail.fr>
2015-03-24 20:18:56 +0100

    supprimé

--- Math2/maths/fonctions/FonctionDerivable.java
+++
@@ -1,6 +0,0 @@
-package maths.fonctions;
-
-public interface FonctionDerivable extends Fonction {
-	
-	public FonctionDerivable derive() ;
-}
--- Math2/maths/fonctions/Integrateur.java
+++
@@ -1,6 +0,0 @@
-package maths.fonctions;
-
-public interface Integrateur {
-	/* interface representant un schema d’integration numerique */
-	public double integre(Fonction f,double a,double b,int n) ;
-}
